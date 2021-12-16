e1d2a51315378c6717d313c0e83192f3dbf1e685
GuardKiwi <jtroubetaris@umass.edu>
2021-12-16 13:16:32 -0500

    y
    y

--- backup-group328/backup-group328/stepcountingfeatures.py
+++ backup-group328/backup-group328/stepcountingfeatures.py
@@ -29,9 +29,10 @@ def filtSignal(window): #x_signal, y_signal, z_signal, timestamps
         z.append(window[2][2][k]) #z_signal[k]
         accel_time.append(window[3][k]) #timestamps[k]
         r = np.sqrt((x[k]**2)+(y[k]**2)+(z[k]**2))
-        print(x[k])
+        #print(x[k])
+        print(window[k])
         accel_mag.append(r)
-        print(accel_mag[k])
+        #print(accel_mag[k])
 
     for i in range(0, len(accel_mag), buffer_window_size - 1):
         buffer = accel_mag[i:i+buffer_window_size]
