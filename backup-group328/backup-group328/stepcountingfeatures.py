5a6b2a7d71b527952be18f51ff3893f3dba821ca
GuardKiwi <jtroubetaris@umass.edu>
2021-12-16 12:50:01 -0500

    fdsg
    gfh

--- backup-group328/backup-group328/stepcountingfeatures.py
+++ backup-group328/backup-group328/stepcountingfeatures.py
@@ -28,7 +28,8 @@ def filtSignal(window): #x_signal, y_signal, z_signal, timestamps
         y.append(window[1][k]) #y_signal[k]
         z.append(window[2][k]) #z_signal[k]
         accel_time.append(window[3][k]) #timestamps[k]
-        accel_mag.append(math.sqrt(x[k]**2+y[k]**2+z[k]**2))
+        r = math.sqrt(x[k]**2+y[k]**2+z[k]**2)
+        accel_mag.append(r)
         print(accel_mag[k])
 
     for i in range(0, len(accel_mag), buffer_window_size - 1):
