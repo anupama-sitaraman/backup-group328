f57e5420a248544eca25416183e6ec6bef4faa96
GuardKiwi <jtroubetaris@umass.edu>
2021-12-16 12:03:25 -0500

    fwadfgwrs
    dsafzxc

--- backup-group328/backup-group328/songproducer.py
+++ backup-group328/backup-group328/songproducer.py
@@ -30,7 +30,7 @@ sys.stdout.flush()
 # -----------------------------------------------------------------------------
 
 print("Reorienting accelerometer data...")
-#sys.stdout.flush()
+sys.stdout.flush()
 reset_vars()
 reoriented = np.asarray([reorient(data[i,1], data[i,2], data[i,3]) for i in range(len(data))])
 reoriented_data_with_timestamps = np.append(data[:,0:1],reoriented,axis=1)
@@ -56,23 +56,23 @@ class_names = ["running", "walking", "speed walking", "sitting","dancing"]
 print("Extracting features and labels for window size {} and step size {}...".format(window_size, step_size))
 sys.stdout.flush()
 
-X = []
-Y = []
+#X = []
+#Y = []
 
 for i,window_with_timestamp_and_label in slidingWindow(data, window_size, step_size):
     window = window_with_timestamp_and_label[:,1:-1]   
     #feature_names, x = extract_features(window)
-    X.append(x)
-    Y.append(window_with_timestamp_and_label[10, -1])
+    #X.append(x)
+    #Y.append(window_with_timestamp_and_label[10, -1])
     
-X = np.asarray(X)
-Y = np.asarray(Y)
-n_features = len(X)
+#X = np.asarray(X)
+#Y = np.asarray(Y)
+#n_features = len(X)
 
 songName = getSong(filtSignal(window)[4])
 print(songName)
     
-print("Finished feature extraction over {} windows".format(len(X)))
-print("Unique labels found: {}".format(set(Y)))
+#print("Finished feature extraction over {} windows".format(len(X)))
+#print("Unique labels found: {}".format(set(Y)))
 print("\n")
 sys.stdout.flush()
